8f938eb76bb3d22ec947bc69970d16c00e7aded9
devsungmin <kimsungmin.dev@gmail.com>
2019-09-07 22:53:24 +0900

    19.09.07(라우터 기능을 뺀 로그인 기능 구현)

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.3.7'
 
+# rails login
+gem 'devise'
 # rails db
 gem 'rails_db'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_27_142749) do
+ActiveRecord::Schema.define(version: 2019_09_07_134658) do
 
   create_table "freeboards", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2019_08_27_142749) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
